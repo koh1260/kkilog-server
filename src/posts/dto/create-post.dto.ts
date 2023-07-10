e6ccd125d224dc775c9cac1bd71582ceca569f7e
Haesung <koh1260@naver.com>
2023-07-10 18:42:02 +0900

    chore: 간단한 수정

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,9 +1,6 @@
-import { PublicScope } from '../enumerate/public-scope';
-
 export class CreatePostDto {
   title: string;
   content: string;
   introdution: string;
   thumbnail: string;
-  publicScope: PublicScope;
 }
