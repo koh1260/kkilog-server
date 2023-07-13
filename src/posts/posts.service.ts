0522719c3f0d837010e8313bfa127d26067e2665
Haesung <koh1260@naver.com>
2023-07-13 22:55:07 +0900

    refactor: update 반환 타입 설정, createPost 반환 타입 변경
    createPost: Promise<void> -> Promise<Post>

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -3,11 +3,11 @@ import { UpdatePostDto } from './dto/update-post.dto';
 import { Post } from './entities/post.entity';
 
 export interface PostsService {
-  createPost(createPostDto: CreatePostDto, email: string): Promise<void>;
+  createPost(createPostDto: CreatePostDto, email: string): Promise<Post>;
   findAll(): Promise<Post[]>;
   findOne(id: number): Promise<Post>;
   findByCategory(categoryId: number): Promise<Post[]>;
-  update(id: number, updatePostDto: UpdatePostDto);
+  update(id: number, updatePostDto: UpdatePostDto): Promise<Post>;
   remove(id: number);
 }
 export const PostsService = Symbol('PostsService');
