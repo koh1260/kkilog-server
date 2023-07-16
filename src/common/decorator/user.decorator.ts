edf96fa7c812109aadcc3cc4fa4fc381f1164718
Haesung <koh1260@naver.com>
2023-07-16 23:35:43 +0900

    chore: loginUser 데코레이터 이름 변경

--- src/common/decorator/user.decorator.ts
+++ src/common/decorator/user.decorator.ts
@@ -1,7 +1,7 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 import { UserInfo } from '../../auth/jwt.strategy';
 
-export const LoginedUser = createParamDecorator(
+export const LoginUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): UserInfo => {
     const request = ctx.switchToHttp().getRequest();
     return request.user;
