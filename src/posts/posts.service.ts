f1fc96056a2186a1c722e92a3f7444092f76316a
Haesung <koh1260@naver.com>
2023-07-20 04:00:06 +0900

    docs: postService, JsDoc 작성

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -3,11 +3,46 @@ import { UpdatePostDto } from './dto/update-post.dto';
 import { Post } from './entities/post.entity';
 
 export interface PostsService {
+  /**
+   * 게시글 작성.
+   * @param createPostDto 게시글 작성에 필요한 데이터
+   * @param email accessToken으로 얻은 회원 email
+   * @return 생성한 게시글
+   */
   createPost(createPostDto: CreatePostDto, email: string): Promise<Post>;
+
+  /**
+   * 모든 게시글 조회.
+   * @return 게시글 목록
+   */
   findAll(): Promise<Post[]>;
+
+  /**
+   * 게시글 한 건 조회.
+   * @param id 게시글 번호
+   * @return 게시글
+   */
   findOne(id: number): Promise<Post>;
+
+  /**
+   * 카테고리별 게시글 전체 조회.
+   * @param categoryId 카테고리 번호
+   * @return 해당 카테고리의 게시글 목록
+   */
   findByCategory(categoryId: number): Promise<Post[]>;
+
+  /**
+   * 게시글 정보 수정.
+   * @param id 게시글 번호
+   * @param updatePostDto 수정할 내용
+   * @return 수정된 게시글
+   */
   update(id: number, updatePostDto: UpdatePostDto): Promise<Post>;
-  remove(id: number);
+
+  /**
+   * 게시글 삭제.
+   * @param id 게시글 번호
+   */
+  remove(id: number): Promise<void>;
 }
 export const PostsService = Symbol('PostsService');
