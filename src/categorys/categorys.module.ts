770e9d1021211c414edd99f1a135fa5ee5177176
Haesung <koh1260@naver.com>
2023-08-02 21:12:32 +0900

    feat: CategoryModule 생성

--- src/categorys/categorys.module.ts
+++ src/categorys/categorys.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CustomTypeOrmModule } from '../common/custom-repository/custom-typeorm-module';
 import { CategorysRepository } from './categorys.repository';
+import { CategorysController } from './categorys.controller';
+import { CategorysService } from './categorys.service';
 
 @Module({
   imports: [CustomTypeOrmModule.forCustomRepository([CategorysRepository])],
+  controllers: [CategorysController],
+  providers: [CategorysService],
 })
 export class CategorysModule {}
