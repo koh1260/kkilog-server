e640a4861cb1746dd9a07b8f1c98057991fe836a
Haesung <koh1260@naver.com>
2023-08-06 16:07:02 +0900

    refactor: postLike enetity 속성 변경

--- src/posts/entities/post-like.entity.ts
+++ src/posts/entities/post-like.entity.ts
@@ -1,16 +1,10 @@
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { BaseModel } from '../../common/base-enttity/base.entity';
 import { User } from '../../users/user.entity';
 import { Post } from './post.entity';
 
 @Entity('post_like')
 export class PostLike extends BaseModel {
-  @Column()
-  user_id!: number;
-
-  @Column()
-  post_id!: number;
-
   @ManyToOne(() => User, { cascade: true, eager: true })
   @JoinColumn({
     name: 'user_id',
