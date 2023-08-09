93734f6cca29a1a4c09c7a2a2ea755d9abc846d4
Haesung <koh1260@naver.com>
2023-08-09 18:39:20 +0900

    test: mock 객체 전수 실제 객체로 변경

--- src/test/service/users.service.spec.ts
+++ src/test/service/users.service.spec.ts
@@ -3,13 +3,9 @@ import { UsersService } from '../../users/users.service';
 import { UsersRepository } from '../../users/users.repository';
 import { CreateUserDto } from '../../users/dto/create-user.dto';
 import { User } from '../../users/user.entity';
-import { BadRequestException } from '@nestjs/common';
-import { DataSource } from 'typeorm';
-
-const mockUsersRepository = {
-  findOneByEmail: jest.fn(),
-  save: jest.fn((value) => value),
-};
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { TestTypeOrmModule } from '../../common/test-database/test-db.module';
+import { CustomTypeOrmModule } from '../../common/custom-repository/custom-typeorm-module';
 
 describe('UsersService', () => {
   let usersService: UsersService;
@@ -17,68 +13,92 @@ describe('UsersService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        UsersService,
-        { provide: UsersRepository, useValue: mockUsersRepository },
+      imports: [
+        TestTypeOrmModule,
+        CustomTypeOrmModule.forCustomRepository([UsersRepository]),
       ],
+      providers: [UsersService],
     }).compile();
 
     usersService = module.get<UsersService>(UsersService);
     usersRepository = module.get<UsersRepository>(UsersRepository);
   });
 
+  it('회원 정보 조회', async () => {
+    // given
+    const user = createUser(52, 'test@test.com', 'nickname');
+    await usersRepository.save(user);
+
+    // when
+    const foundUser = await usersService.getProfile(user.id);
+
+    // then
+    expect(foundUser.id).toEqual(user.id);
+    expect(foundUser.email).toEqual(user.email);
+  });
+
+  it('존재하지 않는 회원 번호로 회원 정보 조회 시 예외 발생', async () => {
+    // given
+    const nonExistUserId = 1225;
+
+    await expect(
+      // when
+      async () => await usersService.getProfile(nonExistUserId),
+    ).rejects.toThrowError(new NotFoundException('존재하지 않는 회원입니다.')); // then
+  });
+
   it('회원가입 성공', async () => {
     // given
-    const createUserDto = createUserDtoFactory.create(
+    const createUserDto = createUserDtoFactory(
       'test@eaxmple.com',
-      'NAME',
       'NICKNAME',
       'PASSWORD',
     );
-    jest.spyOn(usersRepository, 'findOneByEmail').mockResolvedValue(null);
 
     // when
     const user = await usersService.createUser(createUserDto);
 
     // then
-    expect(user.email).toEqual('test@eaxmple.com');
+    expect((await usersService.getProfile(user.id)).email).toEqual(
+      'test@eaxmple.com',
+    );
   });
 
   it('회원가입 이메일 중복', async () => {
     // given
-    const createUserDto = createUserDtoFactory.create(
+    const createUserDto = createUserDtoFactory(
       'test@eaxmple.com',
-      'NAME',
       'NICKNAME',
       'PASSWORD',
     );
-    const user = new User();
-    user.email = createUserDto.email;
-    user.nickname = createUserDto.nickname;
-    user.password = createUserDto.password;
-    jest.spyOn(usersRepository, 'findOneByEmail').mockResolvedValue(user);
+    await usersService.createUser(createUserDto);
 
     // when
-    await expect(async () => {
-      await usersService.createUser(createUserDto);
-    }).rejects.toThrowError(
+    // then
+    await expect(
+      async () => await usersService.createUser(createUserDto),
+    ).rejects.toThrowError(
       new BadRequestException('이미 존재하는 이메일입니다.'),
     );
   });
 });
 
-class createUserDtoFactory {
-  static create(
-    email: string,
-    name: string,
-    nickname: string,
-    password: string,
-  ) {
-    const dto = new CreateUserDto();
-    dto.email = email;
-    dto.nickname = nickname;
-    dto.password = password;
+const createUserDtoFactory = (
+  email: string,
+  nickname: string,
+  password: string,
+) => {
+  const dto = new CreateUserDto();
+  dto.email = email;
+  dto.nickname = nickname;
+  dto.password = password;
 
-    return dto;
-  }
-}
+  return dto;
+};
+
+const createUser = (id: number, email: string, nickname: string) => {
+  const user = User.of(email, nickname, 'password');
+  user.id = id;
+
+  return user;
+};
