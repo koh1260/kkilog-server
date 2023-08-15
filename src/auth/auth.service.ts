420cca5d4374ca4c6f5e62e28770f92303111e71
Haesung <koh1260@naver.com>
2023-08-16 05:47:03 +0900

    refactor: refresh token 유효성 검사 메서드 분리 등

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import {
   BadRequestException,
   Injectable,
   NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersRepository } from '../users/users.repository';
@@ -11,6 +12,12 @@ import { ConfigService } from '@nestjs/config';
 import * as bcrypt from 'bcrypt';
 import { User } from '../users/user.entity';
 
+interface DecodedToken {
+  id: number;
+  iat: number;
+  exp: number;
+}
+
 @Injectable()
 export class AuthService {
   constructor(
@@ -19,23 +26,34 @@ export class AuthService {
     private readonly configService: ConfigService,
   ) {}
 
+  /**
+   * 회원 검증.
+   * @param email 이메일
+   * @param password 비밀번호
+   * @returns request에 삽입될 회원 정보
+   */
   async validateUser(
     email: string,
     password: string,
   ): Promise<UserInfo | null> {
     const user = await this.usersRepository.findOneByEmail(email);
     if (!user) throw new NotFoundException('존재하지 않는 회원입니다.');
+    if (!(user && (await comparePassword(password, user.password))))
+      throw new BadRequestException('비밀번호가 일치하지 않습니다.');
 
-    if (user && (await comparePassword(password, user.password))) {
-      const userInfo: UserInfo = {
-        id: user.id,
-        email: user.email,
-        nickname: user.nickname,
-      };
-      return userInfo;
-    }
-    throw new BadRequestException('비밀번호가 일치하지 않습니다.');
+    const userInfo: UserInfo = {
+      id: user.id,
+      email: user.email,
+      nickname: user.nickname,
+    };
+    return userInfo;
   }
+
+  /**
+   * Refresh Token 생성.
+   * @param user request에 담겨 있는 회원 정보
+   * @returns Refresh Token
+   */
   async generateRefreshToken(user: UserInfo) {
     const payload = { id: user.id };
     return this.jwtService.signAsync(payload, {
@@ -43,29 +61,51 @@ export class AuthService {
     });
   }
 
+  /**
+   * Access Token 생성.
+   * @param user request에 담겨 있는 회원 정보
+   * @returns Access Token
+   */
   async generateAccessToken(user: UserInfo) {
     return this.jwtService.signAsync(user);
   }
 
+  /**
+   * Access Token 재생성
+   * @param userId 회원 번호
+   * @returns Access Token
+   */
   async regenerateAccessToken(userId: number) {
-    const user = this.existUser(await this.usersRepository.findOneById(userId));
+    const user = await this.usersRepository.findOneById(userId);
+    this.assertUserExist(user);
     const userInfo: UserInfo = {
       id: user.id,
       email: user.email,
       nickname: user.nickname,
     };
+
     return await this.generateAccessToken(userInfo);
   }
 
-  async setRefreshToken(userId: number, refresh: string) {
-    const hashedRefreshToken = await this.gethashedRefreshToken(refresh);
-    const refreshTokenExp = await this.getCurrentRefreshTokenExp();
+  /**
+   * Refresh Token을 DB에 저장.
+   * @param userId 회원 번호
+   * @param refresh Refresh Token
+   */
+  async setRefreshToken(userId: number, refreshToken: string) {
+    const hashedRefreshToken = await this.gethashedRefreshToken(refreshToken);
+    // const refreshTokenExp = await this.getCurrentRefreshTokenExp();
     await this.usersRepository.update(userId, {
       refreshToken: hashedRefreshToken,
-      refreshTokenExp: refreshTokenExp,
+      // refreshTokenExp: refreshTokenExp,
     });
   }
 
+  /**
+   * Refresh Token을 해싱.
+   * @param refreshToken Refresh Token
+   * @returns 해싱된 Refresh Token
+   */
   async gethashedRefreshToken(refreshToken: string) {
     const saltRound = this.configService.get<number>('JWT_REFRESH_HASH_SALT');
     // Joi로 환경변수 유효성 검사 마쳐야만 서버가 실행되므로 항상 null이 아님.
@@ -73,38 +113,36 @@ export class AuthService {
     return await bcrypt.hash(refreshToken, saltRound!);
   }
 
-  async getCurrentRefreshTokenExp(): Promise<Date> {
-    const currentDate = new Date();
-    const currentRefreshTokenExp = new Date(
-      currentDate.getTime() +
-        parseInt(
-          // Joi로 환경변수 유효성 검사 마쳐야만 서버가 실행되므로 항상 null이 아님.
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          this.configService.get<string>('JWT_REFRESH_EXPIRATION_TIME')!,
-        ),
-    );
-    return currentRefreshTokenExp;
+  /**
+   * Refresh Token 검증.
+   * @param refreshToken Refresh Token
+   * @returns 디코딩된 Refresh Token
+   */
+  async verifyRefreshToken(refreshToken: string) {
+    try {
+      const decodedToken = this.jwtService.verify<DecodedToken>(refreshToken);
+      const user = await this.usersRepository.findOneById(decodedToken.id);
+      this.assertUserExist(user);
+      await this.valideteRefreshToken(user.refreshToken, refreshToken);
+
+      return decodedToken;
+    } catch (e: any) {
+      throw new UnauthorizedException('유효하지 않은 토큰입니다.');
+    }
   }
 
-  async verifyRefreshToken(refresh: string) {
-    const decodedToken = this.jwtService.verify(refresh);
-    console.log(decodedToken);
-    const user = this.existUser(
-      await this.usersRepository.findOneById(decodedToken.id),
-    );
-
+  private assertUserExist(user: User | null): asserts user is User {
     if (!user) throw new NotFoundException('존재하지 않는 회원입니다.');
-    if (new Date(decodedToken.exp * 1000) < new Date())
-      throw new BadRequestException('만료된 토큰입니다.');
-    if (!(await bcrypt.compare(refresh, user.refreshToken!)))
-      throw new BadRequestException('유효하지 않은 토큰입니다.');
-
-    return decodedToken;
   }
 
-  existUser(user: User | null): User {
-    if (!user) throw new NotFoundException('존재하지 않는 회원입니다.');
-    return user;
+  private async valideteRefreshToken(
+    usersRefresh: string | undefined,
+    refresh: string,
+  ) {
+    if (!usersRefresh)
+      throw new NotFoundException('발급한 토큰이 존재하지 않습니다.');
+    if (!(await bcrypt.compare(refresh, usersRefresh)))
+      throw new UnauthorizedException('유효하지 않은 토큰입니다.');
   }
 
   async login(user: UserInfo) {
