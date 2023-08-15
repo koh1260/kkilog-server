5ca52907f7710a86fd742e754f6e0d4fa4f3b2db
Haesung <koh1260@naver.com>
2023-08-15 21:27:03 +0900

    chore: user 검색 select 속성 제거

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -6,14 +6,12 @@ import { CustomRepository } from '../common/custom-repository/custom-repository'
 export class UsersRepository extends Repository<User> {
   async findOneByEmail(email: string) {
     return await this.findOne({
-      select: ['id', 'email', 'nickname', 'profileImage', 'role'],
       where: { email: email },
     });
   }
 
   async findOneById(id: number) {
     return await this.findOne({
-      select: ['id', 'email', 'nickname', 'profileImage', 'role'],
       where: { id: id },
     });
   }
