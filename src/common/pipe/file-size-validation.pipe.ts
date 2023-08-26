3f0f8a896e04bb7f27acfa3c8627d8934a47a674
Haesung <koh1260@naver.com>
2023-08-26 23:43:31 +0900

    feat: file validation pipe 추가

---
+++ src/common/pipe/file-size-validation.pipe.ts
@@ -0,0 +1,9 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class FileSizeValidationPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    const oneMb = 1000 * 1000 * 5;
+    return value.size < oneMb;
+  }
+}
