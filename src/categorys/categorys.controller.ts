2b9e528ab8fe3e5080b76bba3d325504b8e01b86
Haesung <koh1260@naver.com>
2023-09-13 19:28:10 +0900

    refactor: 카테고리 조회 응답 포맷 수정

--- src/categorys/categorys.controller.ts
+++ src/categorys/categorys.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { CategorysService } from './categorys.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CustomResponse } from '../common/response/custom-reponse';
 
 @Controller('categorys')
 @ApiTags('카테고리 API')
+@ApiBearerAuth()
 export class CategorysController {
   constructor(private readonly categoryService: CategorysService) {}
 
@@ -12,7 +14,9 @@ export class CategorysController {
     summary: '카테고리 전체 조회 API',
     description: '모든 카테고리를 조회한다',
   })
-  findAll() {
-    return this.categoryService.findAll();
+  async findAll() {
+    const categorys = await this.categoryService.findAll();
+
+    return CustomResponse.create(HttpStatus.OK, '카테고리 조회', categorys);
   }
 }
