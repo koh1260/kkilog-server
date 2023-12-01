a4ddd01e58d29cde41e277eed07f69f90e94c753
Haesung <koh1260@naver.com>
2023-12-01 14:22:18 +0900

    test: 회원가입 예외 케이스 추가

--- src/test/service/users.service.spec.ts
+++ src/test/service/users.service.spec.ts
@@ -6,6 +6,8 @@ import { User } from '../../modules/users/user.entity';
 import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { TestTypeOrmModule } from '../db/test-db.module';
 import { CustomTypeOrmModule } from '../../config/typeorm/custom-typeorm-module';
+import { ConflictEmailException } from '../../modules/users/exception/conflictEmail.exception';
+import { ConflictNicknameException } from '../../modules/users/exception/conflictNickname.exception';
 
 describe('UsersService', () => {
   let usersService: UsersService;
@@ -35,6 +37,42 @@ describe('UsersService', () => {
     // then
     expect(foundUser.id).toEqual(user.id);
     expect(foundUser.email).toEqual(user.email);
+  });
+
+  it('이메일 중복 예외 발생', async () => {
+    // given
+    const user = createUser(52, 'test@test.com', 'nickname');
+    await usersRepository.save(user);
+
+    const conflictUserDto = createUserDtoFactory(
+      'test@test.com',
+      'nickname2',
+      'pass',
+    );
+
+    // when
+    // then
+    await expect(async () => {
+      await usersService.createUser(conflictUserDto);
+    }).rejects.toThrowError(new ConflictEmailException());
+  });
+
+  it('닉네임 중복 예외 발생', async () => {
+    // given
+    const user = createUser(52, 'test@test.com', 'conflictnickname');
+    await usersRepository.save(user);
+
+    const conflictUserDto = createUserDtoFactory(
+      'notconflict@test.com',
+      'conflictnickname',
+      'pass',
+    );
+
+    // when
+    // then
+    await expect(async () => {
+      await usersService.createUser(conflictUserDto);
+    }).rejects.toThrowError(new ConflictNicknameException());
   });
 
   it('존재하지 않는 회원 번호로 회원 정보 조회 시 예외 발생', async () => {
