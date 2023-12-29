d63379d1c51c22ac606ea3412423b446199b44ca
Haesung <koh1260@naver.com>
2023-12-30 04:00:37 +0900

    chore: db 쿼리 로깅하지 않도록 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -38,7 +38,6 @@ import { IntercepterModule } from './common/interceptors/intercepter.module';
       entities: [__dirname + '/modules/**/entities/*.entity{.ts,.js}'],
       migrationsRun: true,
       migrations: [__dirname + '/migrations/*{.ts,.js}'],
-      logging: true,
     }),
     WinstonModule.forRoot({
       level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
