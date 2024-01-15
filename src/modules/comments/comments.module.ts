5c1fb991e0877851ca606ab8ea7aa828e4537b73
Haesung <koh1260@naver.com>
2024-01-16 04:02:07 +0900

    refactor: user typeorm repo 제거

--- src/modules/comments/comments.module.ts
+++ src/modules/comments/comments.module.ts
@@ -1,12 +1,6 @@
-import { Module, Post } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CommentsController } from './comments.controller';
-import { CustomTypeOrmModule } from '../../config/typeorm/custom-typeorm-module';
-import { Comment } from './entities/comment-typeorm.entity';
 import { CommentsService } from './comments.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../users/entities/user.entity';
-import { CommentsRepository as CommentTypeormRepo } from './comments-typeorm.repository';
-import { UsersTypeormRepository } from '../users/users-typeorm.repository';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { CommentsRepository } from './comments.repository';
 import { UsersRepository } from '../users/users.repository';
