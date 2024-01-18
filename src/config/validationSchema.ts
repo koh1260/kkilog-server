38f5f0136fecdf36dedac1c33d16a64141d4bb4b
Haesung <koh1260@naver.com>
2024-01-18 21:38:39 +0900

    chore: 환경 변수 validate 수정

--- src/config/validationSchema.ts
+++ src/config/validationSchema.ts
@@ -1,11 +1,7 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
-  DATABASE_HOST: Joi.string().required(),
-  DATABASE_USERNAME: Joi.string().required(),
-  DATABASE_PASSWORD: Joi.string().required(),
-  DATABASE_NAME: Joi.string().required(),
-  DATABASE_PORT: Joi.number().required(),
+  DATABASE_URL: Joi.string().required(),
   JWT_SECRET_KEY: Joi.string().required(),
   JWT_ACCESS_EXPIRATION_TIME: Joi.number().required(),
   JWT_REFRESH_EXPIRATION_TIME: Joi.number().required(),
